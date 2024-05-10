67421330b143798496c34f16dfbda40a5d042506
Francisco Castillo <fr.castillou@duocuc.cl>
2024-05-09 22:11:08 -0400

    Modificado

--- js/validacionProducto.js
+++ js/validacionProducto.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+    $.validator.addMethod("notZero", function (value, element) {
+        return value != "0";
+    }, "Este campo es obligatorio.");
     $('#formProducto').validate({
         rules: {
             nombreproducto: {
@@ -9,40 +12,24 @@ $(document).ready(function () {
             precio: {
                 required: true,
                 number: true,
-                min: 0.01
-            },
-            cantidad: {
-                required: true,
-                number: true,
                 min: 1
             },
-            descripcion: {
+            consola: {
                 required: true,
-                minlength: 5,
-                maxlength: 500
-            }
-        },
-        messages: {
-            nombre: {
-                required: "El nombre del producto es requerido",
-                minlength: "El nombre del producto debe tener al menos 3 caracteres",
-                maxlength: "El nombre del producto debe tener máximo 50 caracteres"
+                notZero: true,
             },
-            precio: {
-                required: "El precio del producto es requerido",
-                number: "El precio del producto debe ser un número",
-                min: "El precio del producto debe ser mayor a 0"
-            },
-            cantidad: {
-                required: "La cantidad del producto es requerida",
-                number: "La cantidad del producto debe ser un número",
-                min: "La cantidad del producto debe ser mayor a 0"
-            },
-            descripcion: {
-                required: "La descripción del producto es requerida",
-                minlength: "La descripción del producto debe tener al menos 5 caracteres",
-                maxlength: "La descripción del producto debe tener máximo 500 caracteres"
-            }
         }
     });
+    $("#formProducto").submit(function (event) {
+        event.preventDefault();
+        if ($("#formProducto").valid()) {
+            $(".alert").remove();
+            $("#formProducto").before("<div class='alert alert-success alert-dismissible fade show'>Producto registrado</div>");
+            $(".alert").append("<button type='button' class='btn-close' data-bs-dismiss='alert'></button>");
+        } else {
+            $(".alert").remove();
+            $("#formProducto").before("<div class='alert alert-danger alert-dismissible fade show'>Error al registrar producto</div>");
+            $(".alert").append("<button type='button' class='btn-close' data-bs-dismiss='alert'></button>");
+        }
+    })
 });
